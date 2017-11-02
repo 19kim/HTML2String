4a105108deb9ae31d01f160d54d30eca48594508
kim19 <kimig009@gmail.com>
2017-11-02 19:07:39 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function() {
+   document.getElementById('btnConvert').addEventListener('click', function() {
+       var textBox = document.getElementById('inputSource');
+
+       console.log(textBox.innerText);
+       document.getElementById('result').innerText = (convertSource(textBox.value));
+   });
+});
+
+function convertSource(src) {
+    return src
+        .replace(/ /g, '&nbsp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/\n/g, '<br />');
+}
